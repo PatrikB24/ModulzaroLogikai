fb899c36939a0c6ced84f4dbe3461557e5e1a202
PatrikB <bekpatrik24@gmail.com>
2024-06-08 11:41:14 +0200

    LepesekProbalkozas

--- src/modulzaro0608logikai/Modulzaro0608Logikai.java
+++ src/modulzaro0608logikai/Modulzaro0608Logikai.java
@@ -1,13 +1,12 @@
-
 package modulzaro0608logikai;
-
 
 public class Modulzaro0608Logikai {
 
-public static char[] palya; //pálya állapota
-public static char[] jelek;
+    public static char[] palya; //pálya állapota
+    public static char[] jelek;
+
     public static void main(String[] args) {
- 
+
         jatek();
     }
 
@@ -16,41 +15,77 @@ public static char[] jelek;
         kiirPalya(); // Kiírjuk a kezdőállapotot
         System.out.println("");
         while (!vege()) { // Amíg nincs vége a játéknak
-           lepes(); // Lépés végrehajtása
+            lepes(); // Lépés végrehajtása
             kiirPalya(); // Kiírjuk az új állapotot
             System.out.println("");
-    }
+            //if(vege()){
+            // break;}
+        }
     }
 
     private static void kezdoAllapot() {
-    palya = new char[]{'X', 'X', 'X', ' ', 'O', 'O', 'O'};
-}
+        palya = new char[]{'X', 'X', 'X', ' ', 'O', 'O', 'O'};
+    }
 
     private static void kiirPalya() {
-         for (int i = 0; i < palya.length; i++) {
-        System.out.print(palya[i] + " ");
-        if ((i + 1) % 3 == 0) { 
-            System.out.print("");
+        for (int i = 0; i < palya.length; i++) {
+            System.out.print(palya[i] + " ");
+            if ((i + 1) % 3 == 0) {
+                System.out.print("");
+            }
         }
+
     }
-    
-}
 
     private static void lepes() {
+
+        boolean xLepett = false;
+        boolean oLepett = false;
+
         for (int i = 0; i < palya.length - 1; i++) {
-            if (palya[i] == 'X' && palya[i + 1] == ' ') {
+            if (palya[i] == 'X' && palya[i + 1] == ' ' && palya[i + 2] == 'O') {
                 palya[i] = ' ';
                 palya[i + 1] = 'X';
-                return;
-            } else if (palya[i] == ' ' && palya[i + 1] == 'O') {
-                palya[i] = 'O';
-                palya[i + 1] = ' ';
-                return;
+                xLepett = true;
+                break;
+            }
+            else {
+            break;}
+        }
+
+        for (int i = palya.length - 1; i > 1; i--) {
+            if (palya[i] == 'O' && palya[i - 1] == ' ' && palya[i - 2] == 'X') {
+                palya[i] = ' ';
+                palya[i - 1] = 'O';
+                oLepett = true;
+                break;
+            }
+        }
+
+        if (xLepett) {
+            for (int i = 0; i < palya.length - 1; i++) {
+                if (palya[i] == 'X' && palya[i + 1] == 'O' && palya[i + 2] == ' ') {
+                    palya[i] = ' ';
+                    palya[i + 2] = 'X';
+                    break;
+                }
+            }
+        }
+
+        if (oLepett) {
+            for (int i = palya.length - 1; i > 0; i--) {
+                if (palya[i] == 'O' && palya[i - 1] == 'X' && palya[i - 2] == ' ') {
+                    palya[i] = ' ';
+                    palya[i - 2] = 'O';
+                    break;
+                }
             }
         }
     }
 
-    private static boolean vege() {
+
+
+private static boolean vege() {
         char[] vegsoAllapot = {'O', 'O', 'O', ' ', 'X', 'X', 'X'};
         for (int i = 0; i < palya.length; i++) {
             if (palya[i] != vegsoAllapot[i]) {
@@ -62,14 +97,4 @@ public static char[] jelek;
 
 
 
-}
-    
-   
-    
-    
-    
-    
-    
-
-
-
+    }
