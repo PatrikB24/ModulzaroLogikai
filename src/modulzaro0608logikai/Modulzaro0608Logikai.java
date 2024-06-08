ae5d838a0707cd83025ef2deefb4348e1deb8fe0
PatrikB <bekpatrik24@gmail.com>
2024-06-08 10:51:12 +0200

    VegtelenLoopLett

--- src/modulzaro0608logikai/Modulzaro0608Logikai.java
+++ src/modulzaro0608logikai/Modulzaro0608Logikai.java
@@ -15,9 +15,11 @@ public static char[] jelek;
         kezdoAllapot(); // Kezdőállapot inicializálása
         kiirPalya(); // Kiírjuk a kezdőállapotot
         System.out.println("");
-     //   while (!vege()) { // Amíg nincs vége a játéknak
+        while (!vege()) { // Amíg nincs vége a játéknak
            lepes(); // Lépés végrehajtása
             kiirPalya(); // Kiírjuk az új állapotot
+            System.out.println("");
+    }
     }
 
     private static void kezdoAllapot() {
@@ -35,13 +37,32 @@ public static char[] jelek;
 }
 
     private static void lepes() {
-for (int i = 0; i < palya.length / 2; i++) {
-            char Jel = palya[i];
-            palya[i] = palya[palya.length - i - 1];
-            palya[palya.length - i - 1] = Jel;
+        for (int i = 0; i < palya.length - 1; i++) {
+            if (palya[i] == 'X' && palya[i + 1] == ' ') {
+                palya[i] = ' ';
+                palya[i + 1] = 'X';
+                return;
+            } else if (palya[i] == ' ' && palya[i + 1] == 'O') {
+                palya[i] = 'O';
+                palya[i + 1] = ' ';
+                return;
             }
         }
     }
+
+    private static boolean vege() {
+        char[] vegsoAllapot = {'O', 'O', 'O', ' ', 'X', 'X', 'X'};
+        for (int i = 0; i < palya.length; i++) {
+            if (palya[i] != vegsoAllapot[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+}
     
    
     
